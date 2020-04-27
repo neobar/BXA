d2eb6088f2190a458c4856a9da9e861f7e7d7cc3
newbanyaya@gmail.com <sxz89@E1-053344.science.psu.edu>
2020-04-27 17:59:49 -0400

    save and load pca bkg pars

--- bxa/sherpa/background/pcaUtil.py
+++ bxa/sherpa/background/pcaUtil.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from sherpa.astro import ui
-from bxa.sherpa.background.pca import PCAFitter
+from bxa.sherpa.background.pca import PCAFitter, PCAModel, get_identity_response
+import json
 
 
 def fitBkgPCA(id=1):
@@ -12,6 +13,30 @@ def fitBkgPCA(id=1):
     ui.set_stat('cash')
     bkgmodel = PCAFitter(id=id)  # An id has to be assigned.
     bkgmodel.fit()
+
+
+def saveBkgPCA(id=1, writeTo='bkgPCA.json'):
+    """
+    Save the best-fit background model to a .json file.
+    """
+    bkgModel = ui.get_bkg_model(id=id)
+    parDict = {p.fullname: p.val for p in bkgModel.pars}
+    with open(writeTo, 'w') as f:
+        json.dump(parDict, f)
+
+
+def loadBkgPCA(id=1, readFrom='bkgPCA.json'):
+    """
+    load PCA background from
+    """
+    with open(readFrom, 'r') as f:
+        parDict = json.load(f)
+    fitter = PCAFitter(id=id)
+    bkgModel = PCAModel('pca{:d}'.format(id), data=fitter.pca)
+    for p in bkgModel.pars:
+        p.val = parDict[p.fullname]
+    idrsp = get_identity_response(1)
+    ui.set_bkg_full_model(idrsp(bkgModel))
 
 
 def fixBkgModel(id=1, freeNorm=True):
