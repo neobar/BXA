20b13233fe7f5cd0f44eb0835ec5d6e6643fb2cd
nuc <sxz89@psu.edu>
2020-11-06 03:22:29 -0500

    only channel

--- bxa/sherpa/background/pcaUtil.py
+++ bxa/sherpa/background/pcaUtil.py
@@ -104,9 +104,7 @@ def loadBkgPCA(id=1, readFrom='bkgPCA.json'):
     """
     load PCA background from
     """
-    ui.set_analysis('channel')
     filter0 = ui.get_filter()
-    ui.set_analysis(id, 'energy')
 
     with open(readFrom, 'r') as f:
         parDict = json.load(f)
@@ -117,10 +115,8 @@ def loadBkgPCA(id=1, readFrom='bkgPCA.json'):
         p.val = parDictRed[p.name]
     idrsp = get_identity_response(id)
     ui.set_bkg_full_model(id, idrsp(bkgModel))
-    ui.set_analysis(id, 'channel')
     ui.ignore()
     ui.notice(filter0)
-    ui.set_analysis(id, 'energy')
 
 
 def fixBkgModel(id=1, freeNorm=True):
