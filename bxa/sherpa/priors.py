509c1a572538593bdf54a5e0d8bae682eee07b2f
Shifu Zhu <sfzhu@ustc.edu.cn>
2023-03-06 22:53:18 +0800

    follow up stream

--- bxa/sherpa/priors.py
+++ bxa/sherpa/priors.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 """
@@ -9,7 +9,7 @@ Copyright: Johannes Buchner (C) 2013-2015
 
 Priors
 """
-from math import log10, isnan, isinf
+from math import log10, erf
 import numpy
 from . import invgauss
 
@@ -17,7 +17,7 @@ def create_uniform_prior_for(parameter):
 	"""
 	Use for location variables (position)
 	The uniform prior gives equal weight in non-logarithmic scale.
-	
+
 	:param parameter: Parameter to create a prior for. E.g. xspowerlaw.mypowerlaw.PhoIndex
 	"""
 	spread = (parameter.max - parameter.min)
@@ -25,14 +25,18 @@ def create_uniform_prior_for(parameter):
 	return lambda x: x * spread + low
 
 def create_jeffreys_prior_for(parameter):
+	"""deprecated name for create_loguniform_prior_for"""
+	return create_loguniform_prior_for(parameter)
+
+def create_loguniform_prior_for(parameter):
 	"""
 	Use for scale variables (order of magnitude)
 	The Jeffreys prior gives equal weight to each order of magnitude between the
 	minimum and maximum value. Flat in logarithmic scale.
-	
+
 	:param parameter: Parameter to create a prior for. E.g. xspowerlaw.mypowerlaw.norm
 
-	It is usually easier to create an ancillary parameter, and link the 
+	It is usually easier to create an ancillary parameter, and link the
 	actual parameter, like so::
 
 		from sherpa.models.parameter import Parameter
@@ -40,6 +44,7 @@ def create_jeffreys_prior_for(parameter):
 		powerlaw.norm = 10**lognorm
 
 	"""
+	assert parameter.min > 0 and parameter.max > 0, ('The limits of "%s" (min and max) are not positive.' % parameter.fullname)
 	low = log10(parameter.min)
 	spread = log10(parameter.max) - log10(parameter.min)
 	return lambda x: 10**(x * spread + low)
@@ -48,18 +53,27 @@ def create_jeffreys_prior_for(parameter):
 def create_gaussian_prior_for(parameter, mean, std):
 	"""
 	Use for informed variables.
-	The Gaussian prior weights by a Gaussian in the parameter. If you 
+	The Gaussian prior weights by a Gaussian in the parameter. If you
 	would like the logarithm of the parameter to be weighted by
 	a Gaussian, create a ancillary parameter (see create_jeffreys_prior_for).
-	
+
 	:param parameter: Parameter to create a prior for. E.g. xspowerlaw.mypowerlaw.PhoIndex
+	:param mean: Mean of the Gaussian
+	:param std: Standard deviation of the Gaussian
 
 	"""
-	from . import invgauss
+	import scipy.stats
 	lo = parameter.min
 	hi = parameter.max
-	f = invgauss.get_invgauss_func(mean, std)
-	return lambda x: max(lo, min(hi, f(x)))
+
+	rv = scipy.stats.norm(mean, std)
+	xlo = rv.cdf(lo)
+	xhi = rv.cdf(hi)
+	def gauss_transform(x):
+		return rv.ppf(x * (xhi - xlo) + xlo)
+
+	return gauss_transform
+
 
 def prior_from_file(filename, parameter):
 	"""
@@ -67,10 +81,10 @@ def prior_from_file(filename, parameter):
 	The file should have two columns: cumulative probability
 	and value, in ascii format. The cumulative probability
 	has to be equally spaced and should exclude 0 and 1.
-	
+
 	Returns a sherpa parameter, a list with that parameter inside,
 	and the prior function.
-	
+
 	If the file only constains a single value, that value is returned
 	along with two empty lists.
 	"""
@@ -79,19 +93,21 @@ def prior_from_file(filename, parameter):
 		parameter.val = float(dist)
 		return float(dist), [], []
 	distz = numpy.array(list(dist[:, 1]) + [dist[-1,1]]*2)
-	deltax = dist[1,0] - dist[0,0]
+	#deltax = dist[1,0] - dist[0,0]
 	n = len(dist)
 	def custom_priorf(x):
 		assert x >= 0
 		assert x <= 1
 		i = int(numpy.floor(x*n))
-		r = distz[i] + (distz[i + 1] - distz[i]) * (x*n - i)
+		r = distz[i] + (distz[i + 1] - distz[i]) * (x * n - i)
 		return r
 	return parameter, [parameter], [custom_priorf]
 
 def create_prior_function(priors = [], parameters = None):
 	"""
-	Combine the prior transformations into a single function for pymultinest.
+	Combine the prior transformations into a single function.
+
+	This assumes factorized (independent) priors.
 
 	:param priors: individual prior transforms to combine into one function.
 		If priors is empty, uniform priors are used on all passed parameters
@@ -99,19 +115,18 @@ def create_prior_function(priors = [], parameters = None):
 		Uniform priors will be created for them.
 	"""
 
-	functions = []
 	if priors == []:
+		functions = []
 		assert parameters is not None, "you need to pass the parameters if you want automatic uniform priors"
 		thawedparmins  = [p.min for p in parameters]
 		thawedparmaxes = [p.max for p in parameters]
-		for low, high, i in zip(thawedparmins, thawedparmaxes, range(ndim)):
+		for low, high in zip(thawedparmins, thawedparmaxes):
 			functions.append(lambda x: x * (high - low) + low)
 	else:
 		functions = priors
-    
+
 	def prior_function(cube, ndim, nparams):
 		for i in range(ndim):
 			cube[i] = functions[i](cube[i])
 
 	return prior_function
-
